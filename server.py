467f68bca99b08883750d0d6082d6b2a418188b6
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-01-18 23:29:19 +0000

    Fix serving of static files

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return app.send_static_file('index.html')
 
 
 @app.route('/reverse', methods=['POST'])
