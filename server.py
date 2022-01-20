367ed20720da1ffe412b932c5d925e9a1664933a
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-01-20 23:22:30 +0000

    get_weatherdata

--- server.py
+++ server.py
@@ -1,17 +1,36 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+from datetime import datetime, timedelta
+import urllib.request
+import os
 
-app = Flask(__name__, static_url_path='')
+LAT = 53.4371319
+LON = -2.2718197
+
+OPEN_WEATHER_API_KEY = os.environ.get("OPEN_WEATHER_API_KEY")
+
+app = Flask(__name__, static_url_path="")
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    return app.send_static_file('index.html')
+    return app.send_static_file("index.html")
 
 
-@app.route('/reverse', methods=['POST'])
+@app.route("/reverse", methods=["POST"])
 def form_example():
-    input = request.form.get('input')
+    input = request.form.get("input")
     print(f"input: {input}")
     output = input[::-1]
     print(f"output: {output}")
     return output
+
+
+@app.route("/weatherdata", methods=["GET"])
+def get_weatherdata():
+    now = datetime.now()
+    yesterday = now - timedelta(days=1)
+    dt = int(yesterday.timestamp())
+    url = f"http://api.openweathermap.org/data/2.5/onecall/timemachine?lat={LAT}&lon={LON}&dt={dt}&units=metric&appid={OPEN_WEATHER_API_KEY}"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    return Response(data, content_type="application/json; charset=utf-8")
